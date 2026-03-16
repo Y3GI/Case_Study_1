4f651373b4b6454d418b19623c88d0432d5a8f5f
Y3GI <boyansnew@gmail.com>
2026-03-16 14:48:23 +0100

    monitoring sec group updated

--- modules/monitoring/sec_groups.tf
+++ modules/monitoring/sec_groups.tf
@@ -35,6 +35,6 @@ resource "aws_security_group" "monitoring_stack_sg" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = [var.private_vpc_cidr]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
