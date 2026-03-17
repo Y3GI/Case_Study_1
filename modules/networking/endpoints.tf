417d4653b69509924e9b9d148f5c017d9863daae
Y3GI <boyansnew@gmail.com>
2026-03-17 21:52:17 +0100

    sts endpoint added

--- modules/networking/endpoints.tf
+++ modules/networking/endpoints.tf
@@ -73,6 +73,15 @@ resource "aws_vpc_endpoint" "cloudwatch_metrics" {
     private_dns_enabled = true
 }
 
+resource "aws_vpc_endpoint" "sts" {
+    vpc_id              = aws_vpc.private.id
+    service_name        = "com.amazonaws.${var.region}.sts"
+    vpc_endpoint_type   = "Interface"
+    subnet_ids          = [aws_subnet.private["lambda_subnet1"].id, aws_subnet.private["lambda_subnet2"].id]
+    security_group_ids  = [aws_security_group.monitoring_endpoint_sg.id]
+    private_dns_enabled = true
+}
+
 resource "aws_vpc_endpoint" "s3"{
     vpc_id = aws_vpc.private.id
     service_name =  "com.amazonaws.${var.region}.s3"
